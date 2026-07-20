d6f7422ef73f0697f72bd03beeb7e5295d6c999d
CavadDeveloper <109869776+CavadDeveloper@users.noreply.github.com>
2026-07-20 16:56:49 +0400

    feat: add category validation DTOs with class-validator configurations

--- src/modules/category/dto/create-category.dto.ts
+++ src/modules/category/dto/create-category.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCategoryDto {}
+import { IsString, IsNotEmpty, IsOptional, IsNumber } from 'class-validator';
+export class CreateCategoryDto {
+  @IsString({
+    message: 'Kateqoriya adı mütləq mətn(string) formatında olmalıdır.',
+  })
+  @IsNotEmpty({ message: 'Kateqoriya adını boş qoymaq olmaz!' })
+  name!: string;
+  @IsString({ message: 'Açıqlama mətn(string)formatında yazılmalıdır.' })
+  @IsOptional()
+  description?: string;
+  @IsNumber({}, { message: 'Parent kateqoriya ID-si rəqəm(number) olmalıdır' })
+  @IsOptional()
+  parentId?: number;
+}
