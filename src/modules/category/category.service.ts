c5dbfddb7af1736283456a833e41e0f9a7f73374
CavadDeveloper <109869776+CavadDeveloper@users.noreply.github.com>
2026-07-21 12:52:26 +0400

    feat:complete category service and controller

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,26 +1,105 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IsNull, Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const category = this.categoryRepository.create(createCategoryDto);
+
+    if (createCategoryDto.parentId) {
+      const parent = await this.categoryRepository.findOne({
+        where: { id: createCategoryDto.parentId },
+        relations: { parent: true },
+      });
+
+      if (!parent) {
+        throw new NotFoundException(
+          'Seçilən valideyn (parent) kateqoriya tapılmadı.',
+        );
+      }
+
+      if (parent.parent) {
+        throw new BadRequestException(
+          'Maksimum 1 mərhələ dərinliyə icazə verilir. Seçilən parent özü alt kateqoriyadır!',
+        );
+      }
+
+      category.parent = parent;
+    }
+
+    return this.categoryRepository.save(category);
+  }
+  async findAll(): Promise<Category[]> {
+    return this.categoryRepository.find({
+      where: { parent: IsNull() },
+      relations: { children: true },
+    });
   }
 
-  findAll() {
-    return `This action returns all category`;
-  }
+  // 3. READ (SINGLE)
+  async findOne(id: number): Promise<Category> {
+    const category = await this.categoryRepository.findOne({
+      where: { id },
+      relations: { parent: true, children: true },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
+    if (!category) {
+      throw new NotFoundException(`ID-si ${id} olan kateqoriya tapılmadı.`);
+    }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+    return category;
   }
+  async update(
+    id: number,
+    updateCategoryDto: UpdateCategoryDto,
+  ): Promise<Category> {
+    const category = await this.findOne(id);
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+    if (updateCategoryDto.parentId) {
+      if (updateCategoryDto.parentId === id) {
+        throw new BadRequestException(
+          'Kateqoriya özü-özünün valideyni ola bilməz!',
+        );
+      }
+
+      const parent = await this.categoryRepository.findOne({
+        where: { id: updateCategoryDto.parentId },
+        relations: { parent: true },
+      });
+
+      if (!parent) {
+        throw new NotFoundException(
+          'Seçilən valideyn (parent) kateqoriya tapılmadı.',
+        );
+      }
+
+      if (parent.parent) {
+        throw new BadRequestException(
+          'Maksimum 1 dərinliyə icazə verilir! Alt kateqoriyanın alt kateqoriyası ola bilməz.',
+        );
+      }
+
+      category.parent = parent;
+    }
+
+    Object.assign(category, updateCategoryDto);
+    return this.categoryRepository.save(category);
+  }
+  async remove(id: number): Promise<{ message: string }> {
+    const category = await this.findOne(id);
+    await this.categoryRepository.remove(category);
+    return { message: 'Kateqoriya Uğurla Silindi' };
   }
 }
